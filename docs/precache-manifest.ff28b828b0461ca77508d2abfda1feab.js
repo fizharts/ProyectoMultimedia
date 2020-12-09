430249d9f87c9e8f1f3c16ce0a456ee08eda684b
julio picazo <jpicazo06@gmail.com>
2020-12-09 13:23:20 -0600

    configurando el docs

---
+++ docs/precache-manifest.ff28b828b0461ca77508d2abfda1feab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9354922c3f6f7a7d44d18bfc75673cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ecd809bfebae9f6c48",
+    "url": "/static/css/main.ec19169e.chunk.css"
+  },
+  {
+    "revision": "4acb6e7093fa0742fedf",
+    "url": "/static/js/2.62eda869.chunk.js"
+  },
+  {
+    "revision": "d1f552577fbd0859131edd51f65f75d8",
+    "url": "/static/js/2.62eda869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ecd809bfebae9f6c48",
+    "url": "/static/js/main.ae0aace1.chunk.js"
+  },
+  {
+    "revision": "ecf78a01c1be7943c8a7",
+    "url": "/static/js/runtime-main.4223b1c3.js"
+  }
+]);
